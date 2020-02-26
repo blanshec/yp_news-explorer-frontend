38b65d121f5bf9e55eaac035c9240732337cbab7
alinaadburshina <alinaabdurshina@gmail.com>
2020-02-26 12:03:30 +0300

    Upload initials

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+/* eslint-disable no-undef */
 // eslint-disable-next-line import/extensions
 import './scripts/script.js';
 import './index.css';
+import Popup from './blocks/popup/popup';
+
+document.querySelector('.header').addEventListener('click', (event) => {
+  if (event.target.classList.contains('header__button')) {
+    const popup = new Popup('');
+    popup.open();
+  }
+});
