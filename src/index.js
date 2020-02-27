ab84a23cb3b916e406ba740d774fbcb5dca5f111
alinaadburshina <alinaabdurshina@gmail.com>
2020-02-27 14:46:09 +0300

    Transfer wrokspace

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-/* eslint-disable no-undef */
 // eslint-disable-next-line import/extensions
-import './scripts/script.js';
+import './scripts/script';
 import './index.css';
-import Popup from './blocks/popup/popup';
-
-document.querySelector('.header').addEventListener('click', (event) => {
-  if (event.target.classList.contains('header__button')) {
-    const popup = new Popup('');
-    popup.open();
-  }
-});
