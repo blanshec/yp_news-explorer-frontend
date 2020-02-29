f00fa53c134a2d5338265bf7ae7c07e92b4b6dd7
alinaadburshina <alinaabdurshina@gmail.com>
2020-02-29 14:12:02 +0300

    Change workspace

--- src/scripts/errorMessages.js
+++ src/scripts/errorMessages.js
@@ -1,6 +1,11 @@
 const ERRORS = {
-  wrongPassword: 'Неправильный пароль',
-  existingUser: 'Такой пользователь уже существует',
+  emailIsInvalid: 'Неверный формат ввода',
+  emailRequired: 'Введите email',
+  passwordIsInvalid: 'Длина пароля должна быть от 8-ми до 30-ти символов',
+  passwordRequired: 'Введите пароль',
+  usernameRequired: 'Введите имя пользователя',
+  usernameIsInvalid: 'Имя пользователя должно быть от 2-ух до 30-ти символов',
+  inputOutOfBound: 'Слишком много полей ввода',
 }
 
 export default ERRORS;
